d7a6947d698c5c7d1f4e6f64616e377fbefd271a
Ann-Atz <annatzkern@gmail.com>
2022-12-26 00:09:46 +0100

    "Landing Page Verbessert"

--- Frontend/educare/src/components/LandingPage/LandingPageContent.js
+++ Frontend/educare/src/components/LandingPage/LandingPageContent.js
@@ -1,10 +1,21 @@
-
+import image from '../image/illustration-landingpage.png';
 
 export default function LandingPageContent() {
     return (
-        <div>
-            <p>Mit dem Kalender von EduCare kann man wichtige Termine als Lehrer, aber auch als ein Elternteil eintragen.
-                Interkulturelle Verständigung ist uns als EduCare sehr wichtig, weshalb wir auch für die Kommunikation mit über 7 Sprachen anbieten um so, Missverständnisse zu vermeiden!</p>
-        </div>
+        <><img src={image} className="landingpage-image" alt="img" /><div className="LandingText">Mit dem Kalender von EduCare kann man wichtige Termine als Lehrer, aber auch als ein Elternteil eintragen.
+            Interkulturelle Verständigung ist uns als EduCare sehr wichtig, weshalb wir auch für die Kommunikation mit über 7 Sprachen anbieten um so, Missverständnisse zu vermeiden!</div>
+            <div className='Wrapper'>
+                <div className='Calendar'>
+                    <form className="Calendar button" action="/Calendar">
+                        <button type="submit">Kalender</button>
+                    </form>
+                </div>
+                <div className='Reports'>
+                    <form className="ReportsButton" action="/ReportsPage">
+                        <button type="submit">Meldungen</button>
+                    </form>
+                </div>
+            </div></>
+
     )
 }
--- Frontend/educare/src/pages/LandingPage.js
+++ Frontend/educare/src/pages/LandingPage.js
@@ -1,18 +1,12 @@
 import '../components/LandingPage/LandingPage.css';
 import '../App.css';
 //import Footer from "../components/Footer/Footer";
-import HomePageContent from '../components/HomePage/HomePageContent';
 import LandingPageContent from '../components/LandingPage/LandingPageContent';
-import image from '../components/image/illustration-landingpage.png';
 
 function LandingPage() {
     return (
         <div>
-            <HomePageContent />
-            <img src= {image} className="landingpage-image" alt="img" />
             <LandingPageContent />
-                <form name='Calender_button' action='/Calendar'><button type="submit">Kalender</button></form>
-                <form name='Reports_button' action='/ReportsPage'><button type="submit">Meldungen</button></form>
             </div>
     );
 }
