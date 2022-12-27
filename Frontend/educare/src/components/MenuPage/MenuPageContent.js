69578879fbe59f535ff3e5db7dc99a30f91e196a
Ann-Atz <annatzkern@gmail.com>
2022-12-27 18:39:57 +0100

    Settings Responsive II

--- Frontend/educare/src/components/MenuPage/MenuPageContent.js
+++ Frontend/educare/src/components/MenuPage/MenuPageContent.js
@@ -3,30 +3,31 @@ import * as BsIcons from "react-icons/bs";
 import * as FiIcons from "react-icons/fi";
 import * as AiIcons from "react-icons/ai";
 import * as CgIcons from "react-icons/cg";
+const istyle = { color: "white", fontSize: "1.3em" }
 
 export const MenuPageContent = [
     {
     title: "Kalender",
     path: "/Calendar",
-    icon: <BsIcons.BsCalendar4Week />,
+    icon: <BsIcons.BsCalendar4Week style={istyle}/>,
     cName: "nav-text",
     },
 {
     title: "Meldungen",
     path: "/ReportsPage",
-    icon: <CgIcons.CgFileDocument />,
+    icon: <CgIcons.CgFileDocument  style={istyle}/>,
     cName: "nav-text",
 },
 {
     title: "LandingPage",
     path: "/LandingPage",
-    icon: <AiIcons.AiOutlineHome />,
+    icon: <AiIcons.AiOutlineHome  style={istyle}/>,
     cName: "nav-text",
 },
 {
     title: "Einstellungen",
     path: "/Settings",
-    icon: <FiIcons.FiSettings />,
+    icon: <FiIcons.FiSettings  style={istyle}/>,
     cName: "nav-text",
 },
 ]
--- Frontend/educare/src/pages/MenuPage.js
+++ Frontend/educare/src/pages/MenuPage.js
@@ -4,7 +4,8 @@ import * as GoIcons from "react-icons/go";
 import * as CgIcons from "react-icons/cg";
 import { MenuPageContent } from "../components/MenuPage/MenuPageContent";
 import '../components/MenuPage/MenuPage.css';
-import { IconContext } from 'react-icons';
+const style = { color: "white", fontSize: "1em" }
+
 
 function MenuPage() {
   const [sidebar, setSidebar] = useState(false);
@@ -13,17 +14,16 @@ function MenuPage() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff', size: '20px' }}>
         <div className='menupage'>
           <Link to='#' className='menu-bars'>
-            <GoIcons.GoThreeBars onClick={showSidebar} />
+            <GoIcons.GoThreeBars onClick={showSidebar} style={style} />
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='menupage-toggle'>
               <Link to='#' className='menu-bars'>
-                <CgIcons.CgClose />
+                <CgIcons.CgClose style={style} />
               </Link>
             </li>
             {MenuPageContent.map((item, index) => {
@@ -38,7 +38,6 @@ function MenuPage() {
             })}
           </ul>
         </nav>
-      </IconContext.Provider>
     </>
   );
 }
